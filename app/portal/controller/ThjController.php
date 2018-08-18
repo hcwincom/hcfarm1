d5b9cbd523532b62991ca24f3bb5164d98315f2d
11223ren <1132974092@qq.com>
2018-08-18 11:41:49 +0800

    提交销售网点和头文件

--- app/portal/controller/ThjController.php
+++ app/portal/controller/ThjController.php
@@ -35,6 +35,8 @@ class ThjController extends HomeBaseController
         $sn=$this->request->param('sn','');
         $sn='18080200';
         $this->assign('sn',$sn);
+        $this->assign('html_flag','goods');
+        $this->assign('html_title','券卡提货');
         return $this->fetch();
     }
     //提货验证
@@ -85,12 +87,15 @@ class ThjController extends HomeBaseController
         if(empty($data['sn']) || empty($data['psw']) || empty($data['uname']) || empty($data['utel']) || empty($data['city'])){
             $this->error('输入错误');
         }
+        $time=time();
         $update=[
             'uname'=>$data['uname'],
             'utel'=>$data['utel'],
             'city'=>$data['city'],
             'address'=>$data['address'],
-            ''
+            'take_time'=>$time,
+            'time'=>$time,
+            'take_dsc'=>$data['take_dsc'], 
         ];
         if(strlen($data['psw'])!=6){
             $this->error('密码错误');
@@ -104,12 +109,61 @@ class ThjController extends HomeBaseController
         }
         //判断状态
         if($info['status']!=3){
-            $this->error('不能提货');
+            $this->error('状态异常，不能提货');
         }
-        //判断时间
-        $time=time();
+        //判断时间 
         if($info['value_time2'] >$time ){
-            
+            $this->error('券卡已过期');
         }
+        //预订时间为空为立即发货 
+        if(!empty($data['get0_time'])){
+            if($info['value_time1'] > $data['get0_time']){
+                $this->error('预订时间太早，不在发货期内');
+           }
+           if($info['value_time2'] < $data['get0_time']){
+               $this->error('预订时间太晚，不在发货期内');
+           }
+           $update['get0_time']=$data['get0_time'];
+        }
+        
+        db('voucher')->where($where)->update($update);
+        $this->success('已提交提货信息，请等待发货，可以在网站查询快递进度');
+    }
+    //
+    //线下网点
+    public function networks()
+    {
+        //获取查询
+        $data=$this->request->param();
+        $m_net=db('network');
+        //获取所有有线下网点的城市
+        $keys=$m_net->distinct('city')->column('city');
+        $citys=db('city')->where('id','in',$keys)->column('id,name');
+        $where=[];
+        if(empty($data['city'])){
+             $data['city']=0;
+        }else{
+             $where['city']=['eq',$data['city']];
+        }
+        if(empty($data['name'])){
+             $data['name']='';
+        }else{
+             $where['name']=['like','%'.$data['name'].'%'];
+        }
+      
+        $list= db('network')->field('p.*,concat(c1.name,c2.name) as city_name')
+        ->alias('p')
+        ->join('cmf_city c2','c2.id=p.city')
+        ->join('cmf_city c1','c1.id=c2.fid')
+        ->where($where)
+        ->paginate(6);
+        // 获取分页显示
+        $page = $list->appends($data)->render(); 
+        
+        $this->assign('page',$page);
+        $this->assign('list',$list);
+        $this->assign('html_flag','networks');
+        $this->assign('html_title','线下网点');
+        return $this->fetch();
     }
 }
